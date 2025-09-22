b23711ba6e531791c0af1973a1b34396bb37edb1
ys kim <tbvjdkfdir@gmail.com>
2025-09-22 17:28:12 +0900

    로컬 호스트로 변경

--- src/Constants/define.h
+++ src/Constants/define.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define IOCPTHREAD_CNT 6
-#define ACCEPT_IP "61.75.14.73"
+#define ACCEPT_IP "127.0.0.1"
 #define ACCEPT_PORT 7000
 #define PACKET_BODY_BUF 1024
 #define ROOM_TITLE_LEN 54
